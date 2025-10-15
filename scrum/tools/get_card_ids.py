ffd636e4acf523d731ea1b9537364799b721b199
amaclay <alex.maclay@gmail.com>
2025-10-15 14:27:08 -0400

    Trim unnecessary code, add comments

--- scrum/tools/get_card_ids.py
+++ scrum/tools/get_card_ids.py
@@ -1,37 +1,29 @@
-import statistics
-import math
-import json
-from datetime import date
+""" Print a pipe-deliimeted list of card short_links for all cards on the board """
+# Set working directory
 import os
-
 os.chdir('/home/pocdart/pocdart_documentation/scrum/tools/')
 
-from trello.db import SprintDBManager
+# Import local modules
+from sprint_utils import load_config
 from trello.api import TrelloAPI
 from trello.board import Board
-from sprint_utils import load_config
 
 # Load configuration
-config = load_config("config.json")
-mysql_config = config['mysql']
-board_config = config['board']
-
+board_config = load_config("config.json")['board']
 # Initialize database manager and Trello API
-sprint_db_manager = SprintDBManager(mysql_config)
 trello_api = TrelloAPI(
     board_id=board_config['board_id'],
     api_key=board_config['api_key'],
     api_token=board_config['api_token']
 )
-
 # Get board data
 board_data = trello_api.get_board_cards()
 board = Board(trello_api, board_data)
 board.extract_cards(calc_sp = False)
-cards = board.get_cards()
+# Compile list of short_links
 card_links = []
-for card in cards:
+for card in board.get_cards():
     card_links += [card.get_short_link()]
-
+# Format and print output
 output = '|'.join(card_links)
 print(output)
